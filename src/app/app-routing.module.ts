5d7c8a5e2ea3ebae28631ad7e8ea26ad3615b9b4
서버 <song1111111@naver.com>
2017-11-21 11:37:09 +0900

    기본 라우터 설정

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+
+const routes: Routes = [
+  {path:'', component:IndexComponent, children:[
+    {path: '', component: HomeComponent},
+    {path: 'jquery', component: JqueryComponent},
+  ]}
+  //참고 : 향후 관리자 생성모듈
+  //{ path:'admin', loadChildren:'app/admin/admin.module#AdminModule}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +16,8 @@ import { JqueryComponent } from './jquery/jquery.component';
     JqueryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
